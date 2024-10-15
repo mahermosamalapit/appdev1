6989a408757fd092dc855d8020be72134cff8767
mahermosamalapit <miamalapitmaganda@gmail.com>
2024-10-15 21:09:14 +0800

    im done completing section on displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Profile from './Profile';
 function App() {
   return (
     <>
       <h1>Welcome to my app</h1>
+      <Profile/>
       <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg"/>
+      
     </>
   );
 }
